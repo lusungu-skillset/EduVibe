30b575065ec1661dae329659988c6c07c107dafb
Shadreck Mkandawire <144344337+shaldremkandawire@users.noreply.github.com>
2025-07-21 18:53:20 +0200

    frontend structure

---
+++ eduvibe/src/components/pages/homepage/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the main content of the homepage.</p>
+    </div>
+  );
+}
