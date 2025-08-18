1445fc4823296c6512ff8e75f69f4c1360f80601
Lusungu Mhango <lusungu-skillset>
2025-08-18 23:49:19 +0200

    updated eslint

--- eduvibe/eslint.config.js
+++ eduvibe/eslint.config.js
@@ -4,6 +4,20 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, react: pluginReact },
+    extends: ["js/recommended", ...pluginReact.configs.flat.recommended],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", 
+    },
+    settings: {
+      react: {
+        version: "detect", 
+      },
+    },
+  },
 ]);
