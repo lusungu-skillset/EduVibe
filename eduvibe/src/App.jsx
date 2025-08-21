0f08721d7f59e0dc69e8d9396a4d6b3fe8a205dc
Shadreck Mkandawire <144344337+shaldremkandawire@users.noreply.github.com>
2025-08-21 20:45:01 +0200

    New app.jsx that renders homepage and header

--- eduvibe/src/App.jsx
+++ eduvibe/src/App.jsx
@@ -1,12 +1,14 @@
 import './index.css'; 
+import Homepage from './components/homepage';
+import Header from './components/Header';
+import PopularCourses from './components/PopularCourses';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
-      <div className="bg-blue-500 text-white p-6 rounded-lg shadow-lg text-center">
-        <h1 className="text-2xl font-bold mb-2 text-gray-500">Hello Team, let's work now!!!</h1>
-        <p className="text-sm">Quapo and misheck lets clack some code🎉</p>
-      </div>
+    <div>
+      <Header />
+      <Homepage/>
+      <PopularCourses/>
     </div>
   );
 }
